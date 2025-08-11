4c453103b086d0c5a923f3e69f84c3ab4f49d397
codegod <ashutheola@gmail.com>
2025-08-11 20:57:10 +0100

    create two .c files attendance and main

--- Readme.md
+++ Readme.md
@@ -1,1 +1,3 @@
 # roijhtr98terwh9itewjnh9itewsjn9ijigfrsigrsi
+
+# Hi Chida, creating a pull request soon, this is a test
---
+++ attendance.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+typedef struct {
+char name[50];
+int id;
+char date[11];
+// Format: YYYY-MM-DD
+char time[6];
+// Format: HH:MM
+char status[10]; // Present, Absent, Late
+} Attendance;
+
+int main(){
+printf("Hello world");
+};
